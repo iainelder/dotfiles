b1be9e758ef8a6cbcc5a4b57205cb6cc7a2d1cbc
Iain Samuel McLean Elder <iain@isme.es>
2020-12-30 20:56:12 +0100

    Self-test at end of installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,12 +4,24 @@ cp .bashrc ~/.bashrc
 
 mkdir --parents ~/tmp
 
+# Apart from daily developer use, git may be required for installing various
+# things from source.
+sudo apt --assume-yes install git
+
+# Bats is used to self-test later. It's also a useful unit testing tool.
+sudo apt --assume-yes install bats
+
+# Install tools for installing other tools that use Python.
 sudo apt --assume-yes install \
 	python3-pip \
 	python3-venv
 pip3 install --user pipx
 
+# Install csvkit, the king of CSV analyzers.
 pipx install csvkit
+
+# Test that everything worked with bats
+bats ../test.bats
 
 echo "All Installed!"
 echo "You have to \`source ~/.bashrc\` to make everything work."
