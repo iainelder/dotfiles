d7dd3a00ea4305c9ae752e83071011a84e037dd7
Iain Samuel McLean Elder <iain@isme.es>
2020-12-30 05:09:27 +0100

    Install csvkit

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,5 +5,7 @@ mkdir --parents ~/tmp
 sudo apt --assume-yes install python3-pip
 pip3 install --user pipx
 
+pipx install csvkit
+
 echo "All Installed!"
 echo "You have to \`source ~/.bashrc\` to make everything work."
