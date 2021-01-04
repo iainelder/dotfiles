f7e196ab3830bf63b253c574ca885565fbd2c07e
Iain Samuel McLean Elder <iain@isme.es>
2021-01-04 21:35:17 +0100

    Add debugging to setpath

--- scripts/setpath.sh
+++ scripts/setpath.sh
@@ -2,6 +2,8 @@
 # Do it specially for Github Actions.
 # See CVE-2020-15228.
 # https://github.com/advisories/GHSA-mfwh-5m23-j46w
+set +eux
+
 if [[ -n "${CI-}" ]]; then
   echo "~/.local/bin" >> "${GITHUB_PATH-}"
 else
