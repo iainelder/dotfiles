6d8e91353e6d2b258275e425a817a81bd6dcb6af
Iain Samuel McLean Elder <iain@isme.es>
2021-01-05 04:34:51 +0100

    Add script to install aws-cli v2

---
+++ scripts/programs/awscli.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eu
+
+# Adapted from official instructions.
+# https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
+
+pushd ~/tmp
+
+curl \
+--url 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' \
+--output awscliv2.zip
+
+unzip awscliv2.zip
+
+./aws/install --install-dir ~/.local/aws-cli --bin-dir ~/.local/bin
+
+popd
+
