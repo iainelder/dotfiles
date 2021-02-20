375d4ce0690f3e6f70f9723f61f48d03586cae36
Iain Samuel McLean Elder <iain@isme.es>
2021-02-20 14:30:03 +0100

    Add Anki installer

---
+++ scripts/programs/anki.bash
@@ -0,0 +1,30 @@
+# https://apps.ankiweb.net/
+# https://github.com/ankitects/anki/releases
+# https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c#gistcomment-2574561
+
+set -euxo pipefail
+
+cd ~/tmp
+
+browser_download_url=$(
+  curl -Ss 'https://api.github.com/repos/ankitects/anki/releases/latest' |
+  jq -r '.assets[] | select(.name | test("linux")) | .browser_download_url'
+)
+
+download_filename=$(
+  curl \
+  --silent \
+  --show-error \
+  --url "$browser_download_url" \
+  --location \
+  --remote-name \
+  --write-out '%{filename_effective}'
+)
+
+tar --extract --bzip2 --file "$download_filename"
+
+extract_folder=$(basename "$download_filename" ".tar.bz2")
+
+cd "$extract_folder"
+
+sudo ./install.sh
