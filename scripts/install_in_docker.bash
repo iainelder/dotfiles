a03f9feea3f89dd53ced98233b524576d9ee0432
Iain Samuel McLean Elder <iain@isme.es>
2021-03-02 14:49:58 +0100

    Install Spotify

---
+++ scripts/install_in_docker.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+program="${1}"
+
+set -euxo pipefail
+
+apt -y update && apt -y install sudo
+
+# Make `apt-get install` work correctly in sudo for packages depending on those
+# such as tzdata and keyboard-configuration that freeze the Docker installation
+# with a prompt.
+export DEBIAN_FRONTEND=noninteractive
+echo "Defaults env_keep += \"DEBIAN_FRONTEND\"" >> /etc/sudoers
+
+bash "${program}"
---
+++ scripts/programs/spotify.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# https://www.spotify.com/es/download/linux/
+
+set -euxo pipefail
+
+cd "$(mktemp --dir)"
+
+sudo apt-get update && sudo apt-get install --yes \
+curl \
+gnupg
+
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg |
+sudo apt-key add - 
+
+echo "deb http://repository.spotify.com stable non-free" |
+sudo tee /etc/apt/sources.list.d/spotify.list
+
+sudo apt-get update && sudo apt-get install --yes spotify-client
+
+spotify --version
