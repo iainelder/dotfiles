45bdd0cb08656967147ce0b91b371ca1d335d7f5
Iain Samuel McLean Elder <iain@isme.es>
2021-03-08 18:36:47 +0100

    Install GnuCash

---
+++ scripts/programs/gnucash.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+cd "$(mktemp --dir)"
+
+sudo apt-get update && sudo apt-get install --yes \
+gnucash
+
+# My desktop language is Spanish, but I use the English localization.
+# Financial language is hard enough to understand without mangled
+# translations such as "Puede escoger un conjunto de ficheros aquí que
+# parece cerrar a sus necesidades".
+# - "seems close to"? - "aproxime"?
+sudo sed \
+--in-place=.bak \
+--expression 's%^Exec=\(.*\)$%Exec=/bin/bash -c "LANGUAGE=en_GB \1"%' \
+/usr/share/applications/gnucash.desktop
