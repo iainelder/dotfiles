95f5bc06664011fc1dad8d644f8f146725f8bcf1
Iain Samuel McLean Elder <iain@isme.es>
2021-03-09 00:20:34 +0100

    Use apt-get stable CLI interface and long opts

--- scripts/install_in_docker.bash
+++ scripts/install_in_docker.bash
@@ -4,7 +4,7 @@ program="${1}"
 
 set -euxo pipefail
 
-apt -y update && apt -y install sudo
+apt-get --yes update && apt-get --yes install sudo
 
 # Make `apt-get install` work correctly in sudo for packages depending on those
 # such as tzdata and keyboard-configuration that freeze the Docker installation
