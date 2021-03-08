72be744955479206c46cbb8e506f1106968d4a1d
Iain Samuel McLean Elder <iain@isme.es>
2021-03-08 18:35:46 +0100

    Install awsls

---
+++ scripts/programs/awsls.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# https://github.com/jckuester/awsls/blob/master/README.md
+
+set -euxo pipefail
+
+cd "$(mktemp --dir)"
+
+sudo apt-get update && sudo apt-get install --yes \
+curl
+
+installer_url="https://raw.githubusercontent.com/jckuester/awsls/master/install.sh"
+
+installer_filename=$(
+  curl \
+  --silent \
+  --show-error \
+  --url "$installer_url" \
+  --location \
+  --remote-name \
+  --write-out '%{filename_effective}'
+)
+
+bash ./${installer_filename} -b ~/.local/bin
+
+~/.local/bin/awsls --version
