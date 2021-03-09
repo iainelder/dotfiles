7cafeb669706507f0683c380858bf0537ef227f9
Iain Samuel McLean Elder <iain@isme.es>
2021-03-09 02:48:32 +0100

    Add docker preparation script and use to install

--- scripts/install_in_docker.bash
+++ scripts/install_in_docker.bash
@@ -1,25 +1,12 @@
 #!/bin/bash
 
-program="${1}"
-
 set -euxo pipefail
 
-apt-get --yes update && apt-get --yes install sudo
+program="${1}"
 
-# Make `apt-get install` work correctly in sudo for packages depending on those
-# such as tzdata and keyboard-configuration that freeze the Docker installation
-# with a prompt.
-echo "export DEBIAN_FRONTEND=noninteractive" >> /etc/profile.d/noninteractive.sh
-echo "Defaults env_keep += \"DEBIAN_FRONTEND\"" >> /etc/sudoers
+script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
-# Create the user's bin directory. Already exists for a normal Ubuntu user.
-mkdir --parents ~/.local/bin
-
-# Create non-root user norm and add it to the passwordless sudo group.
-echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-adduser --disabled-password --gecos '' norm
-adduser norm sudo
-
+"${script_dir}/prepare_docker.bash"
 
 # Run program install script as norm user.
 su --login norm $(realpath "${program}")
