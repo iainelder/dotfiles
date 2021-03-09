4ddb2d4d78e60e106055d1edbb20b47369e8c261
Iain Samuel McLean Elder <iain@isme.es>
2021-03-09 16:37:41 +0100

    Undo prepare docker in local test; doesn't test executablity

--- scripts/programs/az.bash
+++ scripts/programs/az.bash
@@ -9,6 +9,19 @@ curl
 
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-az extension add --name azure-devops
+sudo az extension add --system --upgrade --name azure-devops
 
 az --version
+
+# Fix bug in az devops login
+# https://github.com/Azure/azure-devops-cli-extension/issues/1099
+# https://github.com/Azure/azure-cli/issues/16858
+sudo apt-get install --yes python3-pip
+sudo pip3 install --upgrade pip --target /opt/az/lib/python3.6/site-packages/
+
+# Test command only works interactively.
+
+# Interactive output: "Failed to store PAT using keyring; falling back to file storage."
+# Scripted output: "Unable to use secure credential store in this environment. ... An error occurred. Pip failed with status code 2 for package keyring~=17.1.1. Use --debug for more information."
+# echo "faketoken" | az devops login --debug
+# az devops logout
--- test_local.bash
+++ test_local.bash
@@ -7,4 +7,4 @@ script="${1}"
 docker run -it --rm \
 --mount "type=bind,source=$(pwd),target=/code" \
 ubuntu:20.04 \
-bash -c "/code/scripts/prepare_docker.bash && /code/${script}; bash"
+bash -c "/code/scripts/install_in_docker.bash /code/${script}; bash"
