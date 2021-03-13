a62bdd34d5cda321dc5d36032372baf20d585a3e
Iain Samuel McLean Elder <iain@isme.es>
2021-03-13 22:02:31 +0100

    Generate CI scripts without maintaining separate config

--- generate_ci.bash
+++ generate_ci.bash
@@ -3,13 +3,20 @@
 set -euxo pipefail
 set +x
 
-cfg=".github/workflows/ci.cfg"
 template=".github/workflows/_program.yml.template"
 
-for cifile in $(crudini --get "${cfg}"); do
-  cat "$template" |
-  program_name="$(crudini --get "$cfg" "${cifile}" program_name)" \
-  program_script="$(crudini --get "$cfg" "${cifile}" program_script)" \
+for program in scripts/programs/*.bash; do
+
+  file_basename="$(basename "${program}" .bash)"
+
+  set +e
+  program_name="$(grep --perl-regexp --only-matching '(?<=# Name: ).*$' "${program}")"
+  set -e
+
+  cat "${template}" |
+  program_name="${program_name:-$file_basename}" \
+  program_script="${program}" \
   envsubst \
-  > .github/workflows/"${cifile}"
+  > .github/workflows/"${file_basename}.yml"
+
 done
