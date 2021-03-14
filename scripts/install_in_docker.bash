9810b2acaea3085858a06e35aaa7ddac3fff354f
Iain Samuel McLean Elder <iain@isme.es>
2021-03-14 02:44:43 +0100

    Require installer to be executable

--- scripts/install_in_docker.bash
+++ scripts/install_in_docker.bash
@@ -9,4 +9,4 @@ script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 "${script_dir}/prepare_docker.bash"
 
 # Run program install script as norm user.
-su --login norm $(realpath "${program}")
+su --login norm --command "bash --login -c '$(realpath "${program}")'"
