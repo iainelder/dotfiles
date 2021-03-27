313f33b5b842341110eb2b8b5c7bbc86443f7465
Iain Samuel McLean Elder <iain@isme.es>
2021-03-27 02:20:53 +0100

    Allow running other containers from in the main
    This is not Docker-in-Docker, but sharing the main Docker socket with
    the container.
    
    https://jpetazzo.github.io/2015/09/03/do-not-use-docker-in-docker-for-ci/

--- test_local.bash
+++ test_local.bash
@@ -5,6 +5,7 @@ set -euxo pipefail
 script="${1:-_}"
 
 docker run -it --rm \
+--volume /var/run/docker.sock:/var/run/docker.sock \
 --mount "type=bind,source=$(pwd),target=/code" \
 ubuntu:20.04 \
 bash -c "/code/scripts/install_in_docker.bash ${script}"
