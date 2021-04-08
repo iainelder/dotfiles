4b55f1787c696798a5b8dcf5b52196ff0af89747
Iain Samuel McLean Elder <iain@isme.es>
2021-04-08 23:22:28 +0200

    Add FIXME note for caps lock

--- scripts/profile/capslock.sh
+++ scripts/profile/capslock.sh
@@ -1,3 +1,4 @@
 # Disable caps lock.
 # https://askubuntu.com/questions/574208/deactivate-caps-lock-in-14-04
+# FIXME: this should run once at startup, not per shell session.
 setxkbmap -option caps:none
