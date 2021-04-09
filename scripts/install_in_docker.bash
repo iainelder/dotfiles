fa0cc769d46b2c822912df3c3a1c10c20fc9aa4a
Iain Samuel McLean Elder <iain@isme.es>
2021-04-09 17:59:47 +0200

    Allow CTRL+C in debugging shell

--- scripts/install_in_docker.bash
+++ scripts/install_in_docker.bash
@@ -8,10 +8,16 @@ script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 "${script_dir}/prepare_docker.bash"
 
+# The session-command option is "discouraged" by the manual, but makes CTRL+C
+# work in the interactive shell for debugging. It's unclear what the security
+# risk is. There is some discussion here.
+# https://www.suse.com/support/kb/doc/?id=000017760
+# https://serverfault.com/questions/599678/what-is-the-difference-between-su-command-and-su-session-command
+# https://serverfault.com/a/599827/53319
 if test "_" != "${program}"; then
   # Run program install script as norm user and bash to debug after.
-  su --login norm --command "bash --login -c '/code/${program}'; bash"
+  su --login norm --session-command "bash --login -c '/code/${program}'; bash"
 else
   # Run interactive shell for experimenting.
-  su --login norm --command "bash --login"
+  su --login norm --session-command "bash --login"
 fi
