fa59c03820d8fbc0ef2fd8acae0d021267a9d2af
Iain Samuel McLean Elder <iain@isme.es>
2021-04-09 18:21:09 +0200

    Make rclone installer idempotent

--- programs/rclone/install.bash
+++ programs/rclone/install.bash
@@ -11,6 +11,15 @@ sudo apt-get update && sudo apt-get install --yes \
 curl \
 unzip
 
+set +e
 curl https://rclone.org/install.sh | sudo bash
+install_err=$?
+set -e
+
+# Exit code 3 means already on latest version; 0 means installed successfully.
+# Anything else is a real error.
+if test 3 -ne "${install_err}" && test 0 -ne "${install_err}"; then
+  exit $install_err
+fi
 
 rclone --version
