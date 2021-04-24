6840686d7613aedcc2f57f020718a202f14ea1f8
Iain Samuel McLean Elder <iain@isme.es>
2021-04-24 14:29:42 +0200

    Install pipx system-wide

---
+++ programs/pipx/install.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Name: Pipx
+
+set -euxo pipefail
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+sudo apt-get update && sudo apt-get install --yes \
+python3-pip \
+python3-venv
+
+sudo pip3 install pipx
+
+sudo cp "${script_dir}"/pipx.sh /etc/profile.d/pipx.sh
+
+sudo cp "${script_dir}"/sudoers /etc/sudoers.d/pipx
+
+# Need to source it here for the installation.
+# It will be done automatically at next login.
+source /etc/profile.d/pipx.sh
+
+sudo pipx install csvkit
+
+csvstat --version
---
+++ programs/pipx/pipx.sh
@@ -0,0 +1,2 @@
+export PIPX_HOME=/opt/pipx
+export PIPX_BIN_DIR=/usr/local/bin
