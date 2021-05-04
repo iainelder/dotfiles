b66f0ae5849b0c1918614d7ed18ff1f9d6324ab4
Iain Samuel McLean Elder <iain@isme.es>
2021-05-04 19:51:13 +0200

    Add option for concise Terraform diffs

--- scripts/profile/terraform.sh
+++ scripts/profile/terraform.sh
@@ -1,2 +1,8 @@
 # https://www.terraform.io/docs/cli/config/config-file.html#provider-plugin-cache
 export TF_PLUGIN_CACHE_DIR="$HOME/.terraform.d/plugin-cache"
+
+# Concise diff hides important identifying information in some complex nested
+# data structures. Uncomment this if important info is missing.
+# https://www.terraform.io/upgrade-guides/0-14.html
+# https://github.com/hashicorp/terraform/issues/27139
+# export TF_X_CONCISE_DIFF=0
