d8001e78af19943b751dcaafca73bd076c26f99f
Iain Samuel McLean Elder <iain@isme.es>
2021-05-21 16:38:53 +0200

    Sync wiki repos too

--- bin/sync_all_repos.bash
+++ bin/sync_all_repos.bash
@@ -2,19 +2,25 @@
 
 set -euxo pipefail
 
-cd ~/Repos
+sync_repos() {
+  cd $1
 
-unclean="$(
-  clustergit --branch '' --recursive --exclude=/\.terraform/ |
-  grep ':' |
-  grep -v Clean |
-  awk '{print $1}'
-)"
+  unclean="$(
+    clustergit --branch '' --recursive --exclude=/\.terraform/ |
+    grep ':' |
+    grep -v Clean |
+    awk '{print $1}'
+  )"
 
-for repo in $unclean; do
-  echo "Entering ${repo} in an interactive subshell."
-  echo "Finalize the working copy, git commit, and exit bash."
-  (cd "${repo}" && git status && bash -li)
-done
+  for repo in $unclean; do
+    echo "Entering ${repo} in an interactive subshell."
+    echo "Finalize the working copy, git commit, and exit bash."
+    (cd "${repo}" && git status && bash -li)
+  done
 
-clustergit --branch '' --recursive --exclude=/\.terraform/ --push
+  clustergit --branch '' --recursive --exclude=/\.terraform/ --push
+}
+
+sync_repos ~/Repos
+
+sync_repos ~/Wikis
