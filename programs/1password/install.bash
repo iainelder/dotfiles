809498e1082b02863b0458bf8b487ee16634f371
Iain Samuel McLean Elder <iain@isme.es>
2021-05-31 12:51:27 +0200

    Fix gpg idempotent output

--- programs/1password/install.bash
+++ programs/1password/install.bash
@@ -15,8 +15,8 @@ gpg
 
 # Add the key for the 1Password apt repository.
 curl -sS https://downloads.1password.com/linux/keys/1password.asc |
-sudo gpg --dearmor --output - \
-> /usr/share/keyrings/1password-archive-keyring.gpg
+gpg --dearmor --output - \
+sudo tee /usr/share/keyrings/1password-archive-keyring.gpg
 
 # Add the 1Password apt repository.
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' |
@@ -30,8 +30,8 @@ sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
 sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
 
 curl -sS https://downloads.1password.com/linux/keys/1password.asc |
-sudo gpg --dearmor --output - \
-/usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+gpg --dearmor --output - \
+sudo tee /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
 
 # Install 1Password.
 sudo apt-get update && sudo apt-get --assume-yes install \
