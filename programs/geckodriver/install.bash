a90afdad308aaae7cf02b4ce24932b655cc26f83
Iain Samuel McLean Elder <iain@isme.es>
2021-06-25 18:16:42 +0200

    Install geckodriver

---
+++ programs/geckodriver/install.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Name: geckodriver
+# https://github.com/mozilla/geckodriver
+
+set -euxo pipefail
+
+cd "$(mktemp --dir)"
+
+sudo apt-get update
+
+# Installer dependencies
+sudo apt-get --assume-yes install \
+curl \
+jq
+
+browser_download_url=$(
+  curl -Ss 'https://api.github.com/repos/mozilla/geckodriver/releases/latest' |
+  jq -r '.assets[] | select(.name | test("linux64.tar.gz$")) | .browser_download_url'
+)
+
+download_filename=$(
+  curl \
+  --silent \
+  --show-error \
+  --url "$browser_download_url" \
+  --location \
+  --remote-name \
+  --write-out '%{filename_effective}'
+)
+
+tar --extract --gzip --file "$download_filename"
+
+extract_folder=$(basename "$download_filename" ".tar.bz2")
+
+sudo mv geckodriver /usr/local/bin
+
+geckodriver --version
