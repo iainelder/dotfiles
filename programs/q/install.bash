d259208c0c3158627872999eb7286d872e2a1908
Iain Samuel McLean Elder <iain@isme.es>
2021-09-14 18:06:01 +0200

    Install q

---
+++ programs/q/install.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Name: q
+# https://github.com/harelba/q
+
+set -euxo pipefail
+
+cd "$(mktemp --dir)"
+
+sudo apt-get update
+
+# Installer dependencies
+sudo apt-get --assume-yes install \
+curl \
+jq
+
+browser_download_url=$(
+  curl -Ss 'https://api.github.com/repos/harelba/q/releases/latest' |
+  jq -r '.assets[] | select(.name | test("amd64.deb")) | .browser_download_url'
+)
+
+download_filename=$(
+  curl \
+  --silent \
+  --show-error \
+  --url "$browser_download_url" \
+  --location \
+  --remote-name \
+  --write-out '%{filename_effective}'
+)
+
+sudo apt install "$(realpath "${download_filename}")"
