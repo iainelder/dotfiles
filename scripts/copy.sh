455d3925e0ab196343dd416465e2f0b8c60209ad
Iain Samuel McLean Elder <iain@isme.es>
2021-09-15 19:15:42 +0200

    Add global .gitconfig to dotfiles

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,4 +1,4 @@
-# Set up bash profile.
+ln -sfv "$(realpath .gitconfig)" ~/.gitconfig
 ln -sfv "$(realpath .bashrc)" ~/.bashrc
 ln -sfv "$(realpath .ackrc)" ~/.ackrc
 rm -rf ~/.dotfiles && ln -sfv "$(realpath profile)" ~/.dotfiles
