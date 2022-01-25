36e7044325fbfe5a62f7a3cdff5ddfe74ce80423
Iain Samuel McLean Elder <iain@isme.es>
2022-01-25 13:05:13 +0100

    Add stub for Joplin

---
+++ programs/joplin/install.bash
@@ -0,0 +1,7 @@
+# TODO: install kmod, fuse
+# https://forum.radxa.com/t/modprobe-command-didnt-present-ubuntu-server-18-04/2006/7
+
+# https://joplinapp.org/help/#desktop-applications
+wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash
+
+~/.joplin/Joplin.AppImage
