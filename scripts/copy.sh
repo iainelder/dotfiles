f547c9db47912702aa6b24b84188546d6eff3fe1
Iain Samuel McLean Elder <iain@isme.es>
2022-02-09 13:07:55 +0100

    Don't end file with newline in vim

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,4 +1,5 @@
 ln -sfv "$(realpath .gitconfig)" ~/.gitconfig
 ln -sfv "$(realpath .bashrc)" ~/.bashrc
 ln -sfv "$(realpath .ackrc)" ~/.ackrc
+ln -sfv "$(realpath .vimrc)" ~/.vimrc
 rm -rf ~/.dotfiles && ln -sfv "$(realpath profile)" ~/.dotfiles
