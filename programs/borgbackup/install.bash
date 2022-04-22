bdec82680cce389194f7acdcc889188211a59cbb
Iain Samuel McLean Elder <iain@isme.es>
2022-04-22 17:15:24 +0200

    Install BorgBackup

---
+++ programs/borgbackup/install.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Name: BorgBackup
+# https://borgbackup.readthedocs.io/en/stable/installation.html
+
+set -euxo pipefail
+
+cd "$(mktemp --dir)"
+
+sudo apt-get update
+
+# Installer dependencies
+sudo apt-get --assume-yes install \
+curl \
+jq
+
+browser_download_url=$(
+  curl -Ss 'https://api.github.com/repos/borgbackup/borg/releases/latest' |
+  jq -r '.assets[] | select(.name | test("borg-linux64$")) | .browser_download_url'
+)
+
+download_filename=$(
+  curl \
+  --silent \
+  --show-error \
+  --url "$browser_download_url" \
+  --location \
+  --remote-name \
+  --write-out '%{filename_effective}'
+)
+
+sudo install -T $download_filename /usr/local/bin/borg
+
+borg --version
