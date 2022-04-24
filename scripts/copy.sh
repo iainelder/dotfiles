7aea0f32999ca8ed06f97142a7409689ab814736
Iain Samuel McLean Elder <iain@isme.es>
2022-04-24 18:02:07 +0200

    Add custom dircolors

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -2,4 +2,5 @@ ln -sfv "$(realpath .gitconfig)" ~/.gitconfig
 ln -sfv "$(realpath .bashrc)" ~/.bashrc
 ln -sfv "$(realpath .ackrc)" ~/.ackrc
 ln -sfv "$(realpath .vimrc)" ~/.vimrc
+ln -sfv "$(realpath .dircolors)" ~/.dircolors
 rm -rf ~/.dotfiles && ln -sfv "$(realpath profile)" ~/.dotfiles
