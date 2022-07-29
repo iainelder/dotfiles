ba449e2d2d53c77dea8482bc1be2869640b33e8f
Iain Samuel McLean Elder <iain@isme.es>
2022-07-29 13:46:48 +0200

    Fix ShellCheck name in CI

--- programs/shellcheck/install.bash
+++ programs/shellcheck/install.bash
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Name: Anki
-# https://apps.ankiweb.net/
-# https://github.com/ankitects/anki/releases
-# https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c#gistcomment-2574561
+# Name: ShellCheck
+# https://www.shellcheck.net/
 
 set -euxo pipefail
 
