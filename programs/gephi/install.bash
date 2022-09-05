109c08a8409d323286b4972b468c7594003f4a7d
Iain Samuel McLean Elder <iain@isme.es>
2022-09-05 11:48:31 +0200

    Avoid GitHub Actions runner timeouts in Gephi installer

--- programs/gephi/install.bash
+++ programs/gephi/install.bash
@@ -38,8 +38,33 @@ sudo mv "${extract_folder}" /opt/gephi
 sudo ln -sf /opt/gephi/bin/gephi /usr/local/bin/gephi
 
 # There is no `--version` option. Any of the `org.gephi` modules report the
-# public version.
+# public version, so we parse that out from the modules list.
 #
 # In a graphical environment this actually launches the GUI. The GUI needs to be
 # closed for the version number to appear and this script to complete.
-gephi --modules --list | grep org.gephi.welcome.screen | awk '{print $2}'
+#
+# The gephi command launches a process that may never terminate in time. In my
+# local test environment, it terminates in about 20 seconds. In the GitHub
+# Actions runner, the build job fails because the runner exceeds the maximum
+# execution time of 360 minutes [1].
+#
+# Thanks to Scott on Super User for a solution that terminates immediately after
+# finding the right output. [2]. It uses command grouping syntax [3] to stop
+# Gephi as soon as the version is printed. The grep output continues downstream
+# and the pkill command produces no output.
+#
+# The gephi command launches two new processes with very long command lines that
+# each contain the word "gephi" somewhere.
+#
+# In the local test environment pid 1 may also contain the word "gephi", so
+# limit the search to children of this script's process.
+#
+# [1]: https://github.com/iainelder/dotfiles/actions/runs/2988076611
+# [2]: https://superuser.com/questions/1450928/how-to-kill-a-pipeline-once-some-output-has-occurred
+# [3]: https://www.gnu.org/software/bash/manual/html_node/Command-Grouping.html
+gephi --modules --list \
+| {
+    grep --max-count 1 org.gephi.welcome.screen; \
+    pkill --parent $$ --oldest --full gephi;
+  } \
+| awk '{print $2}'
