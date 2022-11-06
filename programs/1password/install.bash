3446d596953c06b5f59a6bf60725666c26b642dc
Iain Samuel McLean Elder <iain@isme.es>
2022-11-06 16:38:21 +0100

    Don't echo binary files

--- programs/1password/install.bash
+++ programs/1password/install.bash
@@ -11,12 +11,13 @@ sudo apt-get update
 
 sudo apt-get --assume-yes install \
 curl \
-gpg
+gpg \
+moreutils # Provides sponge.
 
 # Add the key for the 1Password apt repository.
 curl -sS https://downloads.1password.com/linux/keys/1password.asc |
 gpg --dearmor --output - |
-sudo tee /usr/share/keyrings/1password-archive-keyring.gpg
+sudo sponge /usr/share/keyrings/1password-archive-keyring.gpg
 
 # Add the 1Password apt repository.
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' |
@@ -25,13 +26,13 @@ sudo tee /etc/apt/sources.list.d/1password.list
 # Add the debsig-verify policy.
 sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
 curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol |
-sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+sudo sponge /etc/debsig/policies/AC2D62742012EA22/1password.pol
 
 sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
 
 curl -sS https://downloads.1password.com/linux/keys/1password.asc |
 gpg --dearmor --output - |
-sudo tee /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+sudo sponge /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
 
 # Install 1Password.
 sudo apt-get update && sudo apt-get --assume-yes install \
