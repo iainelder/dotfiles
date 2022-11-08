29300573843e122327f042e1875617719c58b663
Iain Samuel McLean Elder <iain@isme.es>
2022-11-08 02:00:50 +0100

    Add a hint to fix caplock

--- scripts/profile/capslock.sh
+++ scripts/profile/capslock.sh
@@ -1,4 +1,11 @@
 # Disable caps lock.
 # https://askubuntu.com/questions/574208/deactivate-caps-lock-in-14-04
+#
 # FIXME: this should run once at startup, not per shell session.
+# Try moving this to /etc/profile.d. 
+#
+# Somehow this can actually set the caps lock to be stuck in ON. Maybe because I
+# press the caps lock key between logging in and opening a terminal.
+# 
+# If that happens, use `setxkbmap -option` to undo it.
 setxkbmap -option caps:none
