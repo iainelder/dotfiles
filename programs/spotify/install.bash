9ac75141bd12bf7cd8dc1dee3c8c000588511446
Iain Samuel McLean Elder <iain@isme.es>
2023-01-24 17:12:20 +0100

    Update public key and installation method

--- programs/spotify/install.bash
+++ programs/spotify/install.bash
@@ -11,8 +11,8 @@ sudo apt-get update && sudo apt-get install --yes \
 curl \
 gnupg
 
-curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg |
-sudo apt-key add - 
+curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg |
+sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 
 echo "deb http://repository.spotify.com stable non-free" |
 sudo tee /etc/apt/sources.list.d/spotify.list
