ba554337735738d2a3bbce0d92162d407be80e67
Iain Samuel McLean Elder <iain@isme.es>
2023-02-13 15:01:48 +0100

    Add pyenvlatest function

--- scripts/profile/pyenv.sh
+++ scripts/profile/pyenv.sh
@@ -2,3 +2,8 @@ export PYENV_ROOT="$HOME/.pyenv"
 export PATH=":$PATH:$PYENV_ROOT/bin"
 eval "$(pyenv init -)"
 eval "$(pyenv init --path)"
+
+pyenvlatest() {
+    version_prefix="$1"
+    pyenv install --list | grep -oP "(?<=  )${version_prefix}.*$" | sort --version-sort | tail -1
+}
