2c7e5347530840a250fce55a774387293b9bc633
Iain Samuel McLean Elder <iain@isme.es>
2023-02-13 18:39:50 +0100

    Patch clustergit to ignore empty repos

--- programs/clustergit/install.bash
+++ programs/clustergit/install.bash
@@ -8,20 +8,26 @@ set -euxo pipefail
 cd "$(mktemp --dir)"
 
 sudo apt-get install --yes \
-git
+curl
 
-repo_url="https://github.com/mnagel/clustergit"
-local_path="/opt/clustergit"
+# The fix for empty repos hasn't been merged yet.
+# https://github.com/mnagel/clustergit/pull/44
+browser_download_url="https://raw.githubusercontent.com/ypid/clustergit/fix/no-commits-yet/clustergit"
 
-if ! test -d "${local_path}"; then
-  sudo git clone "${repo_url}" "${local_path}"
-else
-  sudo git -C "${local_path}" pull
-fi
+download_filename=$(
+  curl \
+  --silent \
+  --show-error \
+  --url "$browser_download_url" \
+  --location \
+  --remote-name \
+  --write-out '%{filename_effective}'
+)
 
+# Application dependencies.
 sudo apt-get install --yes \
 python3
 
-sudo ln -sf "${local_path}"/clustergit /usr/local/bin/clustergit
+sudo install "$download_filename" /usr/local/bin/clustergit
 
 clustergit --help
