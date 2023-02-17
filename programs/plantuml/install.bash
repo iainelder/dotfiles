4661498c6190de9ef9489a29bfd5f1411f356594
Iain Samuel McLean Elder <iain@isme.es>
2023-02-17 10:35:00 +0100

    Install PlantUML

---
+++ programs/plantuml/install.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Name: PlantUML
+# https://plantuml.com/faq-install
+
+set -euxo pipefail
+
+cd "$(mktemp --dir)"
+
+sudo apt-get update
+
+# Installer dependencies
+sudo apt-get --assume-yes install \
+curl \
+jq
+
+browser_download_url=$(
+  curl -Ss 'https://api.github.com/repos/plantuml/plantuml/releases/latest' \
+  | jq -r '.assets[] | select(.name | test("^plantuml.jar$")) | .browser_download_url'
+)
+
+download_filename=$(
+  curl \
+  --silent \
+  --show-error \
+  --url "$browser_download_url" \
+  --location \
+  --remote-name \
+  --write-out '%{filename_effective}'
+)
+
+# Application dependencies
+sudo apt-get --assume-yes install \
+default-jre
+
+java -jar plantuml.jar -authors
