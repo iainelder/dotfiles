a778cb205195d4db30b21ca4ab31bfc04f698ccf
Iain Samuel McLean Elder <iain@isme.es>
2023-03-09 16:58:42 +0100

    Handle non-zero exit code on success

--- programs/firefox-open-url-in-container/install.bash
+++ programs/firefox-open-url-in-container/install.bash
@@ -33,4 +33,22 @@ tar --extract --auto-compress --file "$download_filename"
 
 sudo cp "$(find -name launcher.sh)" /usr/local/bin/firefox-container
 
+set +e
 firefox-container --help
+exit_code="$?"
+set -e
+
+# The help option exits with 1 for successful output.
+# Translate it to a 0.
+if test 1 = "$exit_code"; then
+  exit 0
+fi
+
+# If that ever gets fixed so that it exits with 0, detect it so I can fix this script.
+# Translate a 0 to 1 to fail this script and make me fix it.
+if test 0 -ne "$exit_code"; then
+  exit 1
+fi
+
+# Otherwise, the error code was some other non-zero value. Exit with it.
+exit "$exit_code"
