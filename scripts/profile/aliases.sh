f32d9c51c4bcfcacaf63d6febd8817ebc50b0fdb
Iain Samuel McLean Elder <iain@isme.es>
2023-05-17 19:56:17 +0200

    Add grepenv command

--- scripts/profile/aliases.sh
+++ scripts/profile/aliases.sh
@@ -80,3 +80,7 @@ alias copy="xclip -selection c"
 # https://subscription.packtpub.com/book/networking-&-servers/9781782162742/1/ch01lvl1sec11/function-to-prepend-to-environment-variables
 # I include this mostly as a reminder to read at least one book about Bash.
 function prepend() { [ -d "$2" ] && eval $1=\"$2\$\{$1:+':'\$$1\}\" && export $1 ; }
+
+function grepenv() {
+    printenv | grep -P "$1"
+}
