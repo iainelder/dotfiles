f55944be70beabde8284f9071d7876461e732e1b
Iain Samuel McLean Elder <iain@isme.es>
2023-05-22 20:14:33 +0200

    Install DuckDB

---
+++ programs/duckdb/install.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Name: jd
+# https://github.com/duckdb/duckdb/blob/master/README.md
+
+set -euxo pipefail
+
+cd "$(mktemp --dir)"
+
+sudo apt-get update
+
+sudo apt-get --assume-yes install \
+curl \
+jq \
+unzip
+
+browser_download_url=$(
+  curl -Ss 'https://api.github.com/repos/duckdb/duckdb/releases/latest' |
+  jq -r '.assets[] | select(.name == "duckdb_cli-linux-amd64.zip") | .browser_download_url'
+)
+
+download_filename=$(
+  curl \
+  --silent \
+  --show-error \
+  --url "$browser_download_url" \
+  --location \
+  --remote-name \
+  --write-out '%{filename_effective}'
+)
+
+unzip "${download_filename}"
+
+sudo mv duckdb /usr/local/bin
+
+duckdb --version
