5b8e25c2fa2239e28b55e4b5728c4ee34854f0b5
Iain Samuel McLean Elder <iain@isme.es>
2023-06-29 20:27:15 +0200

    Disable direnv hook until I find a use for it

--- scripts/profile/zz_direnv.sh
+++
@@ -1,5 +0,0 @@
-# From Hook documentation: "Make sure it appears even after rvm, git-prompt and
-# other shell extensions that manipulate the prompt."
-# https://direnv.net/docs/hook.html
-
-eval "$(direnv hook bash)"
