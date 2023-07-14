87195e76f8eaf3093a4ab767ee6219d4fd4b4941
Iain Samuel McLean Elder <iain@isme.es>
2023-07-14 11:43:39 +0200

    Reenable direnv hook
    I need to be able to set GH_CONFIG_DIR to use GH CLI features in non-personal
    accounts.
    
    When the [GH CLI](https://github.com/cli/cli/issues/326) supports multiple profiles I may disable direnv again.

---
+++ scripts/profile/zz_direnv.sh
@@ -0,0 +1,5 @@
+# From Hook documentation: "Make sure it appears even after rvm, git-prompt and
+# other shell extensions that manipulate the prompt."
+# https://direnv.net/docs/hook.html
+
+eval "$(direnv hook bash)"
