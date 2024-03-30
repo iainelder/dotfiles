4a35f0326d43e118ddd001bb16ad8468a362fd0d
Iain Samuel McLean Elder <iain@isme.es>
2024-03-30 22:01:34 +0100

    Enable bracketed paste

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -3,4 +3,5 @@ ln -sfv "$(realpath .bashrc)" ~/.bashrc
 ln -sfv "$(realpath .ackrc)" ~/.ackrc
 ln -sfv "$(realpath .vimrc)" ~/.vimrc
 ln -sfv "$(realpath .dircolors)" ~/.dircolors
+ln -sfv "$(realpath .inputrc)" ~/.inputrc
 rm -rf ~/.dotfiles && ln -sfv "$(realpath profile)" ~/.dotfiles
