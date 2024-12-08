2afe41a4e76b5a0f3436b96c7d1ccab425b6b88f
Iain Samuel McLean Elder <iain@isme.es>
2024-12-08 20:35:27 +0100

    Fix pipx installer for Ubuntu 24

--- programs/pipx/install.bash
+++ programs/pipx/install.bash
@@ -2,13 +2,32 @@
 
 # Name: Pipx
 
-set -euxo pipefail
+install20() {
+    set -euxo pipefail
 
-sudo apt-get update && sudo apt-get install --yes \
-python3-pip \
-python3-venv
+    sudo apt-get update && sudo apt-get install --yes \
+    python3-pip \
+    python3-venv
 
-pip3 install --user pipx
+    pip3 install --user pipx
 
-# In normal use the PATH will include the bin folder.
-"$HOME"/.local/bin/pipx --version
+    # In normal use the PATH will include the bin folder.
+    "$HOME"/.local/bin/pipx --version
+}
+
+install24() {
+    set -euxo pipefail
+
+    sudo apt-get update
+    sudo apt-get install pipx
+
+    pipx --version
+}
+
+source /etc/os-release
+
+if [[ $VERSION_ID = "20.04" || $VERSION_ID = "22.04" ]]; then
+    install20
+else
+    install24
+fi
