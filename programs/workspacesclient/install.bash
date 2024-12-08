14308f61d7a902e9403d5199a69a0db5c75b2881
Iain Samuel McLean Elder <iain@isme.es>
2024-12-08 20:11:42 +0100

    Fix AWS Workspaces Client for Ubuntu 24

--- programs/workspacesclient/install.bash
+++ programs/workspacesclient/install.bash
@@ -17,8 +17,10 @@ key="/usr/share/keyrings/workspaces-client-linux-public-key.gpg"
 wget -q -O - https://workspaces-client-linux-public-key.s3-us-west-2.amazonaws.com/ADB332E7.asc \
 | sudo gpg --dearmor --yes --output "$key"
 
-echo "deb [arch=amd64 signed-by=$key] https://d3nt0h4h6pmmc4.cloudfront.net/ubuntu focal main" \
-| sudo sponge /etc/apt/sources.list.d/amazon-workspaces-clients.list 
+source /etc/os-release
+
+echo "deb [arch=amd64 signed-by=$key] https://d3nt0h4h6pmmc4.cloudfront.net/ubuntu $VERSION_CODENAME main" \
+| sudo sponge /etc/apt/sources.list.d/amazon-workspaces-clients.list
 
 sudo apt-get update
 
