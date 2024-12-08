1313b92990e57516d16018677a29ca28d1a69ccd
Iain Samuel McLean Elder <iain@isme.es>
2024-12-08 21:07:21 +0100

    Fix Workspaces install on Ubuntu 24

--- programs/workspacesclient/install.bash
+++ programs/workspacesclient/install.bash
@@ -18,8 +18,17 @@ wget -q -O - https://workspaces-client-linux-public-key.s3-us-west-2.amazonaws.c
 | sudo gpg --dearmor --yes --output "$key"
 
 source /etc/os-release
+case "$VERSION_ID" in
+    "24.04")
+        # The apt repo server gives a 403 error for "noble".
+        codename="jammy"
+        ;;
+    *):
+        codename="$VERSION_CODENAME"
+        ;;
+esac
 
-echo "deb [arch=amd64 signed-by=$key] https://d3nt0h4h6pmmc4.cloudfront.net/ubuntu $VERSION_CODENAME main" \
+echo "deb [arch=amd64 signed-by=$key] https://d3nt0h4h6pmmc4.cloudfront.net/ubuntu $codename main" \
 | sudo sponge /etc/apt/sources.list.d/amazon-workspaces-clients.list
 
 sudo apt-get update
