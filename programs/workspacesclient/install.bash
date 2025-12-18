148d05d74e082f24b4b9130cd00a0c1ebfefe14d
Iain Samuel McLean Elder <iain@isme.es>
2025-12-18 10:10:22 +0000

    workspacesclient: Use official package for noble
    The jammy package stopped working on noble.
    
    The noble package is available again and listed in the installation documentation.

--- programs/workspacesclient/install.bash
+++ programs/workspacesclient/install.bash
@@ -17,18 +17,9 @@ key="/usr/share/keyrings/workspaces-client-linux-public-key.gpg"
 wget -q -O - https://workspaces-client-linux-public-key.s3-us-west-2.amazonaws.com/ADB332E7.asc \
 | sudo gpg --dearmor --yes --output "$key"
 
-source /etc/os-release
-case "$VERSION_ID" in
-    "24.04")
-        # The apt repo server gives a 403 error for "noble".
-        codename="jammy"
-        ;;
-    *):
-        codename="$VERSION_CODENAME"
-        ;;
-esac
+source <(cat /etc/os-release | grep -P '^VERSION_CODENAME=')
 
-echo "deb [arch=amd64 signed-by=$key] https://d3nt0h4h6pmmc4.cloudfront.net/ubuntu $codename main" \
+echo "deb [arch=amd64 signed-by=$key] https://d3nt0h4h6pmmc4.cloudfront.net/ubuntu $VERSION_CODENAME main" \
 | sudo sponge /etc/apt/sources.list.d/amazon-workspaces-clients.list
 
 sudo apt-get update
